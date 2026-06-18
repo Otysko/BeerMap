4038ad9ca387cdc3ffb938906b7969ac424392d4
David Kunčar <david.kuncar@innone.cz>
2026-06-18 13:23:57 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig(() => {
       hmr: process.env.DISABLE_HMR !== 'true',
       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
+      allowedHosts: [
+        'pivni-mapa-david-g4dphbb7f0e6grbp.germanywestcentral-01.azurewebsites.net'
+      ],
     },
   };
 });
